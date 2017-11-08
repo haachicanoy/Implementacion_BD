42efd3b554fe7ddf2540903a49611109dc49c8d6
DFGonzalez <diegofern.88@gmail.com>
2017-11-07 23:04:00 -0500

    Se crean los directorios de las tablas A y B Se crean dos metodos: 	getFiles: para obtener los archivos de un directorio (root = A ó B) 	seqScan: lee los archivos que se encuentran en la lista de rutas que genera getFiles

--- readCSV/src/readCSV_code.java
+++ readCSV/src/readCSV_code.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,9 +14,22 @@ public class readCSV_code {
 	
 	public static void main(String[] args) {
 		
+		List<String> paths = getFiles("A");
+		
+		for (String path : paths) {
+			System.out.println("Loading: "+path);
+			seqScan(path);
+			System.out.println("Loaded: "+path);
+		}
+ 		
+	}
+	
+	public static boolean seqScan(String path){
+		//1048576
+		//400000 repetidos
 		// .csv comma separated values
-		// Test commit Diego :D
-		String fileName = "data.csv";
+		boolean status = false;
+		String fileName = path;//"data.csv";
 		File file = new File(fileName); // TODO: read about File
 		try {
 			Scanner inputStream = new Scanner(file);
@@ -22,11 +38,44 @@ public class readCSV_code {
 				System.out.println(data);
 			}
 			inputStream.close();
+			status = true;
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace();			
 		}
-		
+		return status;
 	}
+	
+	public static List<String> getFiles(String root){
+		String path = root; 
+
+        String files;
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles(); 
+
+        List<String> paths = new ArrayList<>();
+        System.out.println("Paths loading...");
+        
+        for (int i = 0; i < listOfFiles.length; i++) 
+        {
+
+            if (listOfFiles[i].isFile()) 
+            {
+                files = listOfFiles[i].getName();
+                if (files.endsWith(".csv") || files.endsWith(".CSV"))
+                {
+                	if(files.contains(root)){
+                		paths.add(listOfFiles[i].getPath());
+                		System.out.println(files);
+                	}
+                }
+            }
+        }
+        System.out.println("Paths loaded...");
+        return paths;
+        
+    }
+	
+	
 
 }
