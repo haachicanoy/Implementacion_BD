4590484019848d5572149f708339b72431e181a0
haachicanoy <harold22010@gmail.com>
2017-11-07 20:01:06 -0500

    Reading CSV files

---
+++ readCSV/src/readCSV_code.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * readCSV.java
+ * @author HAACHICANOY
+ */
+
+public class readCSV_code {
+	
+	public static void main(String[] args) {
+		
+		// .csv comma separated values
+		String fileName = "data.csv";
+		File file = new File(fileName); // TODO: read about File
+		try {
+			Scanner inputStream = new Scanner(file);
+			while (inputStream.hasNext()) {
+				String data = inputStream.next();
+				System.out.println(data);
+			}
+			inputStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
