902c1df2124641ebc4ba33a3b736cdb6d7698251
haachicanoy <harold22010@gmail.com>
2017-11-09 00:39:22 -0500

    Implementing Node

--- readCSV/src/Main/readCSV_code.java
+++ readCSV/src/Main/readCSV_code.java
@@ -114,3 +114,55 @@ public class readCSV_code {
 		}*/
 	
 }
+
+//Node implementation
+//Taken from: https://www.discoduroderoer.es/listas-enlazadas-en-java/
+
+public class Nodo<T> {
+    
+    private T dato;
+    private Nodo<T> siguiente;
+ 
+    /**
+     * Constructor por defecto
+     */
+    public Nodo(){
+        siguiente=null;
+     }
+ 
+    /**
+     * Le pasamos un dato al nodo
+     * @param p 
+     */
+    public Nodo(T p){
+        siguiente=null;
+        dato = p;
+    }
+    /**
+     * Le pasamos un dato y su siguiente nodo al nodo
+     * @param t Dato a insertar
+     * @param siguiente Su sisguiente nodo
+     */
+    public Nodo(T t, Nodo<T> siguiente){
+        this.siguiente=siguiente;
+        dato = t;
+    }
+     
+    public T getDato() {
+        return dato;
+    }
+ 
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+ 
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+ 
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+     
+}
+
